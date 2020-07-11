17935afdcab9273803911a2568db8bf6b73ca153
jluini <jluini@gmail.com>
2020-07-11 19:56:19 -0300

    Rubocop: last initial offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+#
+# Base application's helper.
+#
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+#
+# Base application's mailer.
+#
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
